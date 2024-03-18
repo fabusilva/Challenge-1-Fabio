271aec2d2e1034d2374f57374e1998ff4a49e07f
Fábio Silva <106494125+fabusilva@users.noreply.github.com>
2024-03-17 23:24:15 -0300

    #6 Adicionando validações na page register

--- frontend/js/Register.js
+++ frontend/js/Register.js
@@ -15,14 +15,20 @@ formulario.addEventListener('submit',(event)=>{
     if(checkFormulario() == false){
         msgErroFormulario.innerHTML = "All form fields must be filled in"
         msgErroFormulario.style.visibility = "visible";
-    }
+    }else
     if(checkPassword() == false){
         msgErroFormulario.innerHTML = "Different password"
         msgErroFormulario.style.visibility = "visible";
-    }
+    }else
     if(checkSizePassword() == false){
         msgErroFormulario.innerHTML = "Password must be at least 8 characters long"
         msgErroFormulario.style.visibility = "visible";
+    }else if(checkEmailSaved() == true){
+        msgErroFormulario.innerHTML = "This email has already been registered"
+        msgErroFormulario.style.visibility = "visible";
+
+    } else{
+        saveUser();
     }
 })
 
@@ -58,4 +64,28 @@ function checkSizePassword(){
     } else{
         return false;
     }
+}
+function checkEmailSaved(){
+    const key = email.value;
+    console.log(`Key localStorage: ${key}`)
+    if(localStorage.getItem(key) === null){
+        return false
+    } else{
+        return true
+    }
+}
+function saveUser(){
+    const fname = fistname.value;
+    const lname = lastname.value;
+    const date = birthdate.value;
+    const ct = country.value;
+    const cty = city.value;
+    const e_mail = email.value;
+    const pass = password.value;
+    const pass2 = password2.value;
+
+    const usuario = {"fistname": fname, "lastname": lname, "birthdate": date, "country": ct, "city": cty, "email": e_mail, "password": pass, "password2": pass2};
+    const key = e_mail;
+    localStorage.setItem(key, JSON.stringify(usuario));
+    window.location.href = "../pages/Login.html";
 }
